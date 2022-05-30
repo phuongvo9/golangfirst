59082f7852c87b04d59aea812d67af6494702297
phuongvo9 <huyphuong4116@gmail.com>
2022-05-30 21:16:16 +0700

    test: correct test case

--- hello_test.go
+++ hello_test.go
@@ -13,7 +13,7 @@ func TestHello(t *testing.T) {
 	}
 	t.Run("says hello to people", func(t *testing.T) {
 		got := Hello("Phuong")
-		want := "Hello, PhuongV"
+		want := "Hello, Phuong"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello world' if no name is supplied", func(t *testing.T) {
