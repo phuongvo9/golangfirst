8ad600bfbd938d1699ca33f3d00944f4cd13e0aa
phuongvo9 <huyphuong4116@gmail.com>
2022-05-30 21:05:59 +0700

    test: refactor with testTB Helper

--- hello_test.go
+++ hello_test.go
@@ -5,19 +5,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	t.Run("says hello to people", func(t *testing.T) {
-		got := Hello("Phuong")
-		want := "Hello, Phuong"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+	t.Run("says hello to people", func(t *testing.T) {
+		got := Hello("Phuong")
+		want := "Hello, PhuongV"
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello world' if no name is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello World"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 }
